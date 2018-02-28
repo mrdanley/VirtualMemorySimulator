2592d07a42d87c95ff30de0fed7f7e93495c25f5
Daniel <ledaniel89@gmail.com>
2018-02-28 01:42:59 +0000

    calculated page table size
    page size is 256B
    virtual address is 16 bits
    offset is 8 bits
    physical address is 12 bits
    # of page table entries is 2^(virtual address(16) - offset(8) = 8) so 2^8 = 256

--- src/VirtualPageTable.java
+++ src/VirtualPageTable.java
@@ -1,4 +1,8 @@
 public class VirtualPageTable{
   private int[] PageTableEntries; //VirtualPage# as indices
   //table entry - V | R | D | PageFrame#
+  
+  public VirtualPageTable(){
+    PageTableEntries = new int[256];
+  }
 }
