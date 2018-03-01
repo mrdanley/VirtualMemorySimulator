60a4d1fbebc9be498602d0c12c289903463b1a2f
Daniel <ledaniel89@yahoo.com>
2018-02-28 17:08:58 -0800

    added constructors and get/set function definitions

--- src/VirtualPageTable.java
+++ src/VirtualPageTable.java
@@ -1,8 +1,52 @@
 public class VirtualPageTable{
-  private int[] PageTableEntries; //VirtualPage# as indices
+  private PageTableEntry[] vpt; //VirtualPage# as indices
   //table entry - V | R | D | PageFrame#
-  
+
   public VirtualPageTable(){
-    PageTableEntries = new int[256];
+
+  }
+  public PageTableEntry getPTE(int index){
+
+  }
+  public VirtualPageTable(){
+    PageTableEntries = new int[16];
+  }
+}
+
+class PageTableEntry{
+  private int validBit;
+  private int refBit;
+  private int dirtyBit;
+  private String pageFrameNum;
+
+  public PageTableEntry(){
+
+  }
+  public PageTableEntry(int v, int r, int d, String pfn){
+
+  }
+  public void setValid(int v){
+
+  }
+  public void setRef(int r){
+
+  }
+  public void setDirty(int d){
+
+  }
+  public void setPageFrame(String p){
+
+  }
+  public int getValid(){
+
+  }
+  public int getRef(){
+
+  }
+  public int getDirty(){
+
+  }
+  public String getPageFrame(){
+
   }
 }
