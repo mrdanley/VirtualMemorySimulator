f107d07392421a97212c3b040ce9d7de5df8f41d
Daniel <ledaniel89@yahoo.com>
2018-02-28 16:46:31 -0800

    added constructors and get/set function defintions

--- src/TLBCache.java
+++ src/TLBCache.java
@@ -1,5 +1,53 @@
-//table entry - VirtualPage# | V | R | D | PageFrame#
-//must be scanned on every lookup
 public class TLBCache{
-  private int[] TlbEntries = new int[8];
+  private TLBEntry[] TlbEntries;
+
+  public TLBCache(){
+    TlbEntries = new TLBEntry[8];
+  }
+}
+
+class TLBEntry{
+  private String virtualPageNum;
+  private int validBit;
+  private int refBit;
+  private int dirtyBit;
+  private String pageFrameNum;
+  
+  public TLBEntry(){
+    virtualPageNum = "";
+    validBit = 0;
+    refBit = 0;
+    dirtyBit = 0;
+    pageFrameNum = "";
+  }
+  public void setVirtualPage(String v){
+
+  }
+  public void setValid(int v){
+
+  }
+  public void setRef(int r){
+
+  }
+  public void setDirty(int d){
+
+  }
+  public void setPageFrame(String p){
+
+  }
+  public String getVirtualPage(){
+
+  }
+  public int getValid(){
+
+  }
+  public int getRef(){
+
+  }
+  public int getDirty(){
+
+  }
+  public String getPageFrame(){
+
+  }
 }
