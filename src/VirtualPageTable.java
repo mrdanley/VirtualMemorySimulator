ca51086142ad251e1310d7153b698969b7fa94bd
itpwang <itpwang@gmail.com>
2018-02-28 17:38:28 -0800

    update VirtualPageTable & PageTableEntry classes

--- src/VirtualPageTable.java
+++ src/VirtualPageTable.java
@@ -3,14 +3,12 @@ public class VirtualPageTable{
   //table entry - V | R | D | PageFrame#
 
   public VirtualPageTable(){
-
+    vpt = new PageTableEntry[16];
   }
   public PageTableEntry getPTE(int index){
-
+    return vpt[index];
   }
-  public VirtualPageTable(){
-    PageTableEntries = new int[16];
-  }
+  
 }
 
 class PageTableEntry{
@@ -20,33 +18,36 @@ class PageTableEntry{
   private String pageFrameNum;
 
   public PageTableEntry(){
-
+    this(0,0,0,"");
   }
   public PageTableEntry(int v, int r, int d, String pfn){
-
+    validBit = v;
+    refBit = r;
+    dirtyBit = d;
+    pageFrameNum = pfn;
   }
   public void setValid(int v){
-
+    validBit = v;
   }
   public void setRef(int r){
-
+    refBit = r;
   }
   public void setDirty(int d){
-
+    dirtyBit = d;
   }
   public void setPageFrame(String p){
-
+    pageFrameNum = p;
   }
   public int getValid(){
-
+    return validBit;
   }
   public int getRef(){
-
+    return refBit;
   }
   public int getDirty(){
-
+    return dirtyBit;
   }
   public String getPageFrame(){
-
+    return pageFrameNum;
   }
 }
