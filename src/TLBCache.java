0d94441132ef312536767822371c2b13decc7f59
jimontgomery808 <jimontgomery808@gmail.com>
2018-02-28 17:35:15 -0800

    added TLBCachce.java function definitions

--- src/TLBCache.java
+++ src/TLBCache.java
@@ -1,11 +1,12 @@
 public class TLBCache{
+  private final int TLB_SIZE = 8;
   private TLBEntry[] tlb;
 
   public TLBCache(){
-
+    tlb = new TLBEntry[TLB_SIZE];
   }
   public TLBEntry getTLBEntry(int index){
-
+    return tlb[index];
   }
 }
 
@@ -20,36 +21,40 @@ class TLBEntry{
 
   }
   public TLBEntry(String vpn, int v, int r, int d, String pfn){
-
+    virtualPageNum = vpn;
+    validBit = v;
+    refBit = r;
+    dirtyBit = d;
+    pageFrameNum = pfn;
   }
   public void setVirtualPage(String v){
-
+    virtualPageNum = v;
   }
   public void setValid(int v){
-
+    validBit = v;
   }
   public void setRef(int r){
-
+    refBit = r;
   }
   public void setDirty(int d){
-
+    dirtyBit = d;
   }
   public void setPageFrame(String p){
-
+    pageFrameNum = p;
   }
   public String getVirtualPage(){
-
+    return virtualPageNum;
   }
   public int getValid(){
-
+    return validBit;
   }
   public int getRef(){
-
+    return refBit;
   }
   public int getDirty(){
-
+    return dirtyBit;
   }
   public String getPageFrame(){
-
+    return pageFrameNum;
   }
 }
