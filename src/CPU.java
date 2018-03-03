cfc28e9206f61e51707880c91aed7da3108d2476
Daniel <ledaniel89@gmail.com>
2018-03-03 07:16:57 -0800

    added main algorithm

--- src/CPU.java
+++ src/CPU.java
@@ -27,29 +27,94 @@ public class CPU{
       BufferedReader bufferedReader = new BufferedReader(fileReader);
 			StringBuffer stringBuffer = new StringBuffer();
 
-			String instruction,virtualAddress,offset,physicalAddress;
-      int data;
+			String instruction,virtualAddress,offset,vpn,pfn;
+      int rBitCounter = 0, tlbIndex;
       //read in all contents of testfile
 			while ((instruction = bufferedReader.readLine()) != null) {
         //take in next line as vpn
         virtualAddress = bufferedReader.readLine();
-        String vpn = virtualAddress.substring(0,2);
-        int tlbIndex = tlb.getTLBEntryIndex(vpn);
+        vpn = virtualAddress.substring(0,2);
+        offset = virtualAddress.substring(2);
+        // tlbIndex = tlb.getTLBEntryIndex(vpn);
         if(instruction.equals("0")){
+          //read from memory location
           do{
-            physicalAddress = mmu.read(tlb,pt,vpn);
-            if(physicalAddress.equals("")){//clock page replacement algorithm
-              os.pageReplacement(pt,pm,vpn);
-            }else{//pageframe returned, combine with offset and access ram
-              offset = virtualAddress.substring(2,4);
-              System.out.println("Address "+physicalAddress+offset);
+            tlbIndex = mmu.checkTLB(tlb,vpn);
+            if(tlbIndex != -1){//hit
+              mmu.setRef(tlb,pt,tlbIndex);
+              System.out.println("address is "+tlb.getTLBEntry(tlbIndex).getPageFrame()+offset);
               break;
+            }else{//soft miss
+              ptIndex = mmu.checkPT(pt,vpn);
+              if(ptIndex != -1){//hit
+                mmu.setRef(pt,ptIndex);
+                if(tlb.isFull()){//fifo to get tlb index to replace
+                  tlbIndex = mmu.fifo();
+                }else{
+                  tlbIndex = tlb.getStackPtr();
+                  tlb.incrStackPtr();
+                }
+                mmu.copyFromPTtoTLB(tlb,pt,tlbIndex,ptIndex);
+                System.out.println("address is "+tlb.getTLBEntry(tlbIndex).getPageFrame()+offset);
+                break;
+              }else{//hard miss
+                ramIndex = os.clockAlgorithm(pt);
+                if(mmu.checkDirty(pt,ramIndex)){
+                  os.writeRamPageToDisk(pm,ramIndex);
+                }
+                os.writeDiskPageToRam(pm,pt,ramIndex,vpn);
+              }
             }
           }while(true);
         }else{//"1" encountered
-        //CONTINUE HEREEEEE
-          data = Integer.parseInt(bufferedReader.readLine());
-          physicalAddress = mmu.write(tlb,pt,virtualAddress,data);
+          //get dataString from file
+          dataString = bufferedReader.readLine();
+          //write to memory location
+          do{
+            tlbIndex = mmu.checkTLB(tlb,vpn);
+            if(tlbIndex != -1){//hit
+              mmu.setRef(tlb,pt,tlbIndex);
+              //OS write data to ram[PT[vpn]][offset]
+              pfn = tlb.geTLBEntry(Integer.parseInt(vpn,16)).getPageFrame();
+              ramIndex = Integer.parseInt(pfn,16);
+              os.writeToRamPage(pm,ramIndex,dataString,offset);
+              //set dirty bit
+              mmu.setDirty(tlb,pt,tlbIndex);
+              System.out.println("address is "+tlb.getTLBEntry(tlbIndex).getPageFrame()+offset);
+              break;
+            }else{//soft miss
+              ptIndex = mmu.checkPT(pt,vpn);
+              if(ptIndex != -1){//hit
+                mmu.setRef(pt,ptIndex);
+                if(tlb.isFull()){//fifo to get tlb index to replace
+                  tlbIndex = mmu.fifo();
+                }else{
+                  tlbIndex = tlb.getStackPtr();
+                  tlb.incrStackPtr();
+                }
+                mmu.copyFromPTtoTLB(tlb,pt,tlbIndex,ptIndex);
+                pfn = tlb.geTLBEntry(tlbIndex).getPageFrame();
+                ramIndex = Integer.parseInt(pfn,16);
+                os.writeToRamPage(pm,ramIndex,dataString,offset);
+                //set dirty bit
+                mmu.setDirty(tlb,pt,tlbIndex);
+                System.out.println("address is "+tlb.getTLBEntry(tlbIndex).getPageFrame()+offset);
+                break;
+              }else{//hard miss
+                ramIndex = os.clockAlgorithm(pt);
+                if(mmu.checkDirty(pt,ramIndex)){
+                  os.writeRamPageToDisk(pm,ramIndex);
+                }
+                os.writeDiskPageToRam(pm,pt,ramIndex,vpn);
+              }
+            }
+          }while(true);
+        }
+
+        rBitCounter++;
+        if(rBitCounter == 20){
+          os.resetRefBit(tlb,pt);
+          rBitCounter = 0;
         }
 			}
 			fileReader.close();
