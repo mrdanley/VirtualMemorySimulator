f4f2773b99640f6968981d9f2ae662c11bd75576
Daniel <ledaniel89@gmail.com>
2018-03-03 07:17:54 -0800

    added functions to handle physical memory and read and write to disk

--- src/OS.java
+++ src/OS.java
@@ -6,27 +6,45 @@ import java.io.FileReader;
 import java.io.IOException;
 
 public class OS{
+  private CircularLinkedList ptIndices;
+
   public OS(){
   }
   public OS(PhysicalMemory pm,VirtualPageTable pt){
+    clock = new LinkedList();//initialize linked list
     String vpn,pfn;
     for(int i=0;i<16;i++){
       pfn = (Integer.toHexString(i)).toUpperCase();
+      pt.getPTEntries(i) = new PTEntry(1,0,0,pfn);
+      //add 0 because hex 0-15 is 0-F, need 4 more bits for 8 bit vpn
       vpn = "0" + pfn;
-      pt.getPTEntries()[i] = new PTEntry(1,0,0,pfn);
       loadPage(i,vpn,pm);
+      //add vpn index to clock
+      ptIndices.add(i);
     }
-    clockPtr = 0;
   }
-  private void pageReplacement(VirtualPageTable pt,PhysicalMemory pm,String diskReadVPN){
-    //write dirty page to disk
-    String ramWriteOutVPN = "0"+Integer.toHexString(pm.getClockPtr());
+  public int clockAlgorithm(VirtualPageTable pt){
+    //choose page to replace using clock algorithm
+    boolean notFound = true;
+    int currentIndex;
+    do{
+      currentIndex = ptIndices.getNext();
+      if(pt.getPTEntry(currentIndex).getRef()==1){
+        pt.getPTEntry(currentIndex).setRef(0);
+      }else{
+        return ptIndices.remove();
+      }
+    }while(true);
+  }
+  public void writeRamPageToDisk(PhysicalMemory pm,int ramIndex){
+    String diskPageName = Integer.toHexString(ramIndex);
+    if(ramWriteOut.length()==1) ramWriteOut = "0"+ ramWriteOut;
     try {
-      FileWriter fileWriter = new FileWriter(new File("../page_files/"+ramWriteOutVPN+".pg"),false);
+      FileWriter fileWriter = new FileWriter(new File("../page_files/"+diskPageName+".pg"),false);
       BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
       StringBuffer dataString = new StringBuffer("");
-      for(int i=0;i<pm.getRam()[pm.getClockPtr()].length;i++){
-        dataString.append(pm.getRam()[pm.getClockPtr()][i]+"\n");
+      for(int i=0;i<pm.getRam(ramIndex).length;i++){
+        dataString.append(pm.getRam(ramIndex)[i]+"\n");
       }
       bufferedWriter.write(dataString.toString());
       bufferedWriter.close()
@@ -34,21 +52,29 @@ public class OS{
     }catch (IOException e) {
       e.printStackTrace();
     }
-    //load new page to ram
-    loadPage(pm.getClockPtr(),diskReadVPN,pm);
-    pt.getPTEntries()[Integer.parseInt(diskReadVPN,16)].setValid(1);
-    pt.getPTEntries()[Integer.parseInt(diskReadVPN,16)].setRef(0);
-    pt.getPTEntries()[Integer.parseInt(diskReadVPN,16)].setDirty(0);
-    pt.getPTEntries()[Integer.parseInt(diskReadVPN,16)].setPageFrame(Integer.toHexString(pm.getClockPtr()));
+  }
+  public void writeDiskPageToRam(PhysicalMemory pm,VirtualPageTable pt,int ramIndex,String vpn){
+    try {
+      FileReader fileReader = new FileReader(new File("../page_files/"+vpn+".pg"));
+      BufferedReader bufferedReader = new BufferedReader(fileReader);
+      StringBuffer stringBuffer = new StringBuffer();
 
-    //move pointer for ram
-    int newClockPtr;
-    if(pm.getClockPtr()==15){
-      newClockPtr = 0;
-    }else{
-      newClockPtr = pm.getClockPtr() + 1;
+      String dataString;
+      int pageLineIndex = 0;
+      //read in all contents of testfile
+      while ((dataString = bufferedReader.readLine()) != null) {
+        pm.getRam(ramIndex)[pageLineIndex++] = Integer.parseInt(dataString);
+      }
+      bufferedReader.close();
+      fileReader.close();
+    }catch (IOException e) {
+      e.printStackTrace();
     }
-    pt.setClockPtr(newClockPtr);
+    String pageFrame = Integer.toHexString(ramIndex);
+    if(pageFrame.length()==1) pageFrame = "0"+pageFrame;
+    int ptIndex = Integer.parseInt(vpn,16);
+    pt.getPTEntry(ptIndex).setPageFrame(pageFrame);
+    pt.getPTEntry(ptIndex).setDirty(0);
   }
   private void loadPage(int ramIndex,String vpn,PhysicalMemory pm){
     try {
@@ -60,12 +86,24 @@ public class OS{
       int pageLineIndex = 0;
       //read in all contents of testfile
       while ((dataString = bufferedReader.readLine()) != null) {
-        pm.getRam()[ramIndex][pageLineIndex++] = Integer.parseInt(dataString);
+        pm.getRam(ramIndex)[pageLineIndex++] = Integer.parseInt(dataString);
       }
       bufferedReader.close();
       fileReader.close();
     }catch (IOException e) {
       e.printStackTrace();
+    }
+  }
+  public void writeToRamPage(PhysicalMemory pm,int ramIndex,String dataString,String offset){
+    pm.getRam(ramIndex)[offset] = Integer.parseInt(dataString);
+  }
+  public void resetRefBit(TLBCache tlb,VirtualPageTable pt){
+    for(int i=0;i<tlb.getStackPtr();i++){
+      tlb.getTLBEntry(i).setRef(0);
+    }
+    for(int i=0;i<ptIndices.getCounter();i++){
+      pt.getPTEntry(ptIndices.getData()).setRef(0);
+      ptIndices = ptIndices.getNext();
     }
   }
 }
